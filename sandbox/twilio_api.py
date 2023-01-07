7c610ed8f3f7554b30c3efc5a71ed0f1d1ba0bcd
Armaan Kapoor <103453421+ak2k2@users.noreply.github.com>
2023-01-07 18:16:02 -0500

    Update twilio_api.py

--- sandbox/twilio_api.py
+++ sandbox/twilio_api.py
@@ -7,16 +7,16 @@ from twilio.rest import Client
 
 # Find your Account SID and Auth Token at twilio.com/console
 # and set the environment variables. See http://twil.io/secure
-account_sid = 'ACe28e047e148974ee302aa476f2107d63'
-auth_token = 'b27762321b6ba6d2f8b9fa9e8e669cdd'
+account_sid = ''
+auth_token = ''
 
 client = Client(account_sid, auth_token)
 
 message = client.messages \
     .create(
-         body='This is Futura:',
-         from_='+12076871838',
-         to='+19736092074'
+         body='',
+         from_='',
+         to=''
      )
 
 print(message.sid)
